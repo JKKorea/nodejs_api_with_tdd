b9a7be020032d9c99362aea6884140a0002d1fd6
jkkorea <jongkun7987@gmail.com>
2017-07-18 10:59:05 +0900

    limit 값이 없으면 10으로 하고 Int형으로 변환하고 숫자가 아니면 400 상태로 넘기기는 코드 추가

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ var users = [
 app.use(morgan('dev'));
 
 app.get('/users', function(req, res){
-	res.json(users);
+	req.query.limit = req.query.limit || 10;
+	const limit = parseInt(req.query.limit, 10); // "2"를 int의 10진수로 변환
+	if (Number.isNaN(limit)){
+		return res.status(400).end();
+	}
+	res.json(users.slice(0, limit));
 });
 
 app.listen(3000, ()=>{
