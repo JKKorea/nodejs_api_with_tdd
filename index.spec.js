db3122eadfb78f5154db3546f6c16a178764b931
jkkorea <jongkun7987@gmail.com>
2017-07-18 10:59:46 +0900

    limit에 대한 테스트 코드 작성.

--- index.spec.js
+++ index.spec.js
@@ -10,8 +10,26 @@ describe('Get /users는', ()=> {
             .end((err, res) => {
                 res.body.should.be.instanceOf(Array);
                 done();
-            })
-        })
-    })
-    
+            });
+        });
+
+        it('최대 limit 갯수만큼 응답한다. ', (done)=>{
+            request(app)
+                .get('/users?limit=2')
+                .end((err, res) => {
+                    res.body.should.have.lengthOf(2);
+                    done();
+            });
+        });
+    });
+
+    describe('실패시', ()=>{
+        it('limit이 숫자형이 아니면 400을 응답한다. ', (done)=>{
+        request(app)
+            .get('/users?limit=two')
+            .expect(400)  // supertest에서 제공하는 상태 check 함수.
+            .end(done);  // 위의 done의 방식과 같은 것.
+        });
+    });
+
 })
