e6110e8e217e76dbaa480b6c8397886adb9d9aa9
jkkorea <jongkun7987@gmail.com>
2017-07-18 11:38:20 +0900

    테스트 케이스를 통과하기위한 라우팅 함수 작성.

--- index.js
+++ index.js
@@ -24,8 +24,16 @@ app.get('/users/:id', function(req, res){
 
 	const user = users.filter((user) => user.id === id)[0];
 	if (!user) return res.status(404).end();
-	
+
 	res.json(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+	const id = parseInt(req.params.id,10);
+	if (Number.isNaN(id)) return res.status(400).end();
+	
+	users = users.filter(user => user.id !== id);  // 삭제하는 효과
+	res.status(204).end();
 });
 
 app.listen(3000, ()=>{
